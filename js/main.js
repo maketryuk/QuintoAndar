38ecfe52f33683ebd68dc7b9e5ad42a5af13c55a
RomanR301 <roman.sav2nd@gmail.com>
2021-07-15 21:14:40 +0300

    filters

--- js/main.js
+++ js/main.js
@@ -1,91 +1,61 @@
-
-$(function ($) {
-  var CheckboxDropdown = function CheckboxDropdown(el) {
-    var _this = this;
-
-    this.isOpen = false;
-    this.$el = $(el);
-    this.$label = this.$el.find('.dropdown-label span');
-    this.$applyBtn = this.$el.find('.dropdown-apply');
-    this.$clearBtn = this.$el.find('.dropdown-clear');
-    this.$resetBtn = this.$el.find('.btn-reset');
-    this.$inputs = this.$el.find('[type="checkbox"]');
-    this.$label.parent().on('click', function (e) {
-      e.preventDefault();
-
-      _this.toggleOpen();
-    });
-    this.$applyBtn.on('click', function (e) {
-      _this.onCheckApply();
-    });
-    this.$clearBtn.on('click', function (e) {
-      _this.onCheckClear();
-    });
-    this.$resetBtn.on('click', function (e) {
-      e.stopPropagation();
-
-      _this.onCheckReset();
-    });
-  };
-
-  CheckboxDropdown.prototype.onCheckApply = function () {
-    var checked = this.$el.find(':checked');
-    this.isOpen = false;
-    this.$el.removeClass('on');
-
-    if (checked.length <= 0) {
-      this.$label.html('').parent().attr('class', 'dropdown-label default');
-    } else if (checked.length === 1) {
-      this.$label.html(checked.next('label').text()).parent().attr('class', 'dropdown-label one-selected');
-    } else {
-      this.$label.html('&nbsp (' + checked.length + ')').parent().attr('class', 'dropdown-label multiple-selected');
-    }
-  };
-
-  CheckboxDropdown.prototype.onCheckClear = function () {
-    var checked = this.$el.find(':checked');
-    checked.prop('checked', false);
-  };
-
-  CheckboxDropdown.prototype.onCheckReset = function () {
-    var checked = this.$el.find(':checked');
-    this.$label.html('').parent().attr('class', 'dropdown-label default');
-    checked.prop('checked', false);
-  };
-
-  CheckboxDropdown.prototype.toggleOpen = function (forceOpen) {
-    var _this = this;
-
-    if (!this.isOpen || forceOpen) {
-      this.isOpen = true;
-      this.$el.addClass('on');
-      $(document).on('click', function (e) {
-        if (!$(e.target).closest('[data-control]').length) {
-          _this.toggleOpen();
-        }
+let front = {
+  init: function () {
+    this.events();     
+    var swiper = new Swiper(".estate-hero-card-slider", {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: false,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      pagination: {
+        el: ".swiper-pagination"
+      }
+    }); 
+  },
+  events: function () {
+      let self = this;
+      
+      var buttons = document.getElementsByClassName('estate-hero-card__link');
+      Array.prototype.forEach.call(buttons, function (b) {
+        b.addEventListener('mousedown', createRipple);
       });
-    } else {
-      this.isOpen = false;
-      this.$el.removeClass('on');
-      $(document).off('click');
-    }
-  };
+      $('.sort-dropdown__trigger').click(function () {
+        $('.sort-dropdown-list').slideToggle();
+      });
+      $('.burger-wrapper').click(function () {
+        $('.burger').toggleClass('burger--active');
+        $('body').toggleClass('lock');
+        $('.header-navbar').toggleClass('header-navbar_active');
+      });
+      $('#other-field').focus(function () {
+        $('#other').prop("checked", true);
+      });
+      $('#h-search').keyup(function () {
+          validateSearch()
+      });
 
-  var checkboxesDropdowns = document.querySelectorAll('[data-control="checkbox-dropdown"]');
-
-  for (var i = 0, length = checkboxesDropdowns.length; i < length; i++) {
-    new CheckboxDropdown(checkboxesDropdowns[i]);
+      $(document).on('click', '.search__reset', function () {
+        $('#h-search').val('');
+        $(this).removeClass('active');
+      });
+      function validateSearch() {
+        let inputField = $('#h-search').val();
+        let length = inputField.length;
+        if (length > 0) {
+          $('.search__reset').addClass('active');
+        } else {
+          $('.search__reset').removeClass('active');
+        }
+      }
   }
+};
+
+jQuery(function () {
+  front.init();
 });
 
-/***/ }),
-/* 3 */
-/***/ (function(module, exports) {
-
-var buttons = document.getElementsByClassName('estate-hero-card__link');
-Array.prototype.forEach.call(buttons, function (b) {
-  b.addEventListener('mousedown', createRipple);
-});
 
 function createRipple(e) {
   if (this.getElementsByClassName('ripple').length > 0) {
@@ -101,69 +71,54 @@ function createRipple(e) {
   circle.classList.add('ripple');
 }
 
-var swiper = new Swiper(".estate-hero-card-slider", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  loop: false,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  },
-  pagination: {
-    el: ".swiper-pagination"
+$(document).on('click', '.dropdown-label', function () {
+  $(".dropdown").removeClass('on');
+  $(this).parent().addClass('on');
+});
+
+$(document).on("click", function(event){
+  var $trigger = $(".dropdown");
+  if($trigger !== event.target && !$trigger.has(event.target).length){
+    $(".dropdown").removeClass('on');
+  }            
+});
+
+$(document).on('click', '.dropdown-apply', function () {
+  let checked = $(this).parent().parent().find(':checked');
+  let parentLabel = $(this).parent().parent().parent();
+    if (checked.length <= 0) {
+      parentLabel.find('.default-value').html('').parent().attr('class', 'dropdown-label default');
+    } else if (checked.length === 1) {
+      parentLabel.find('.default-value').html(checked.next('label').text()).parent().attr('class', 'dropdown-label one-selected');
+    } else {
+      parentLabel.find('.default-value').html('&nbsp (' + checked.length + ')').parent().attr('class', 'dropdown-label multiple-selected');
+    }
+    $('.dropdown').removeClass('on');
+});
+
+$(document).on('click', '.dropdown-clear', function () {
+  let checked = $(this).parent().parent().find(':checked');
+  checked.prop('checked', false);
+})  
+
+$(document).on('click', '.dropdown-label .icon-close', function (e) {
+  $(this).parent().parent().attr('class','dropdown-label default').find('.default-value').html('');
+  e.stopPropagation();
+});
+
+$(document).on('click','.pricing .dropdown-apply', function(){
+  let minPrice = $('#min-price').val();
+  let maxPrice = $('#max-price').val();
+  if ($('#min-price').val() > 0 && $('#max-price').val() > 0) {
+    $('.pricing .dropdown-label').attr('class', 'dropdown-label one-selected');
+    $('.pricing .dropdown-label span').html('Aluguel R$' + minPrice + '-' + maxPrice)
+  } else if ($('#min-price').val() === '' && $('#max-price').val() > 0) {
+    $('.pricing .dropdown-label').attr('class', 'dropdown-label one-selected');
+    $('.pricing .dropdown-label span').html('Aluguel a partir de R$' + maxPrice)
+  } else if ($('#min-price').val() > 0 && $('#max-price').val() === '') {
+    $('.pricing .dropdown-label').attr('class', 'dropdown-label one-selected');
+    $('.pricing .dropdown-label span').html('Aluguel até R$' + minPrice)
+  } else {
+    $('.pricing .dropdown-label').attr('class', 'dropdown-label default');
   }
-});
-$(function () {
-  $('.sort-dropdown__trigger').click(function () {
-    $('.sort-dropdown-list').slideToggle();
-  });
-});
-
-/***/ }),
-/* 4 */
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-/* 5 */
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-/* 6 */
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-/* 7 */
-/***/ (function(module, exports) {
-
-$(document).on('click', '.search__reset', function () {
-  $('#h-search').val('');
-});
-
-/***/ }),
-/* 8 */
-/***/ (function(module, exports) {
-
-$(function () {
-  $('.burger-wrapper').click(function () {
-    $('.burger').toggleClass('burger--active');
-    $('body').toggleClass('lock');
-    $('.header-navbar').toggleClass('header-navbar_active');
-  });
-});
-
-/***/ }),
-/* 9 */
-/***/ (function(module, exports) {
-
-$('#other-field').focus(function () {
-  $('#other').prop("checked", true);
-});
-
-/***/ })
-/******/ ]);
+})
